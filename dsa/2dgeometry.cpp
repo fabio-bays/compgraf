f8bcb84fbc8966d5242dbae0a8550ec69d32a1f8
Fábio Bays de Araujo <bays.fabio@gmail.com>
2025-09-16 11:44:22 -0300

    update 2dgeometry.cpp

--- dsa/2dgeometry.cpp
+++ dsa/2dgeometry.cpp
@@ -15,6 +15,7 @@ class TwoDHalfEdgeGeometry
         struct vertex *ve;
         struct half_edge *nexthe;
         struct half_edge *prevhe;
+        struct half_edge *twin;
         struct face *fa;
     };
 
@@ -23,16 +24,43 @@ class TwoDHalfEdgeGeometry
         struct half_edge *he;
     };
 
-    std::vector<struct vertex> vx_list;
-    std::vector<struct half_edge> he_list; // Will store just one of the half-edges pair
-    std::vector<struct face> fa_list;
+    /* Half-edges vector stores just one of each half-edges pair. The vector indexes
+    acts as the edge ID*/
+    std::vector<struct vertex *> vx_list;
+    std::vector<struct half_edge *> he_list; 
+    std::vector<struct face *> fa_list;
 
     void new_vertex(int x, int y)
     {
-        
+        struct vertex *new_vx = new struct vertex;
+        new_vx->x = x;
+        new_vx->y = y;
+        new_vx->he = nullptr;
+
+        vx_list.push_back(new_vx);
+    }
+
+    void new_half_edge(struct vertex **point_to_this)
+    {
+        struct half_edge *new_he = new struct half_edge;
+        new_he->ve = *point_to_this;
     }
 
     void new_face(int vx_idx)
+    {
+        struct face *new_fa = new struct face;
+        new_fa->he = nullptr;
+
+        fa_list.push_back(new_fa);
+    }
+
+    /* 1. Save the vertexes; 2. Access the vertex by indexing the vector;
+    3. Create half-edge if not already created and save it; 4. Create its twin
+    5. Return to step 2 while there are remaining vertexes*/
+    TwoDHalfEdgeGeometry()
+    {
+
+    }
 
 
 };
